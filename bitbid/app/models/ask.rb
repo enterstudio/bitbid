ab92178ce3b7c9acc4fd27fec5ececaf09f9af98
fhur <fernando@authy.com>
2014-04-16 23:09:28 -0500

    made ask and bids be created at the same date

--- bitbid/app/models/ask.rb
+++ bitbid/app/models/ask.rb
@@ -5,10 +5,12 @@ class Ask < ActiveRecord::Base
     def Ask.fetch
         cexio = CexIO.new
         asks = cexio.order_book['asks']
+        now = Time.now
         asks.each do |a|
             ask = Ask.new
             ask.price = a[0]
             ask.amount = a[1]
+            ask.created_at = now
             ask.save
         end
     end
--- bitbid/app/models/bid.rb
+++ bitbid/app/models/bid.rb
@@ -2,12 +2,14 @@ class Bid < ActiveRecord::Base
 
     def Bid.fetch
         cexio = CexIO.new
-        asks = cexio.order_book['bids']
-        asks.each do |a|
-            ask = Bid.new
-            ask.price = a[0]
-            ask.amount = a[1]
-            ask.save
+        bids = cexio.order_book['bids']
+        now = Time.now
+        bids.each do |a|
+            bid = Bid.new
+            bid.price = a[0]
+            bid.amount = a[1]
+            bid.created_at = now
+            bid.save
         end
     end
 
