412f36065b3fab4e25d79566010fb866685d691f
fhur <fernando@authy.com>
2014-04-22 09:18:27 -0500

    Almost ready cex_trader
    - Missing implementation for TradeContext
    - Constructor is a bit dirty, probably will have to refactor
    - TODO: write tests

--- trader/cex_trader.rb
+++ trader/cex_trader.rb
@@ -5,39 +5,65 @@ class CexTrader
   # @param {CEX::API} api an instance of CEX::API properly configured for use
   #                       The instance should be setup for buy, sell, account balance
   #                       and open orders
-  def initialize(api)
+  def initialize(api:nil, sleep_period: 30, log: nil)
     @should_terminate = false
     @api = ap
+    @sleep_period = sleep_period
+    @log = log
   end
 
   # calling the start methods starts the trader, i.e. begins trading
   def start
-    # TODO
+    @log.info "starting trader"
     @should_terminate = false
+    iterate()
   end
 
-  def loop
+  # iterates while @should_terminate is false
+  #
+  # The CexTrader takes several steps in the following order
+  # 1. Fetches al data from the API
+  # 2. Calculates if a buy should be made
+  #   => places the buy
+  # 3. Calculates if a sell should be made
+  #   => places the sell
+  # 4. sleeps and re-iterates
+  def iterate
     while @should_terminate == false do
-      # first update all the data variables so that there is always a 'fresh' state
-      # at least as 'fresh' as possible
-      trade_context = update_data()
-      if should_buy(trade_context)
-        buy_order = calc_buy_order(trade_context)
-        place_buy_order(buy_order[0],buy_order[1])
-      end
-      if should_sell(trade_context)
-        sell_order = calc_sell_order(trade_context)
-        place_sell_order(sell_order[0],sell_order[1])
+      begin
+        # first update all the data variables so that there is always a 'fresh' state
+        # at least as 'fresh' as possible
+        trade_context = get_trade_context()
+        @log.debug "Trade context obtained"
+        @log.debug(trade_context)
+
+        # given a trade_context, our task now is to figure out if a buy should be placed
+        if should_buy(trade_context)
+          @log.info "should_buy is true"
+          buy_order = calc_buy_order(trade_context)
+          place_buy_order(buy_order[0],buy_order[1])
+        end
+        # now, lets figure out if a sell should be made (first buy then sell)
+        if should_sell(trade_context)
+          @log.info "should sell is true"
+          sell_order = calc_sell_order(trade_context)
+          place_sell_order(sell_order[0],sell_order[1])
+        end
+        sleep(@sleep_period)
+      rescue => err
+        @log.error("Error while iterating")
+        @log.error(error)
       end
     end
   end
 
   # calls the api to update the trades, bids, asks, balance data
-  def update_data
-
+  def get_trade_context
+    return TradeContext.new(@api)
   end
 
   # this method stops the trader and cancels all active trades
+  # TODO not sure if this method is useful right now, maybe in the near future it will be removed
   def terminate
     @should_terminate = true
   end
@@ -49,7 +75,7 @@ class CexTrader
   # @param {array} bids   an array of current open bids
   # @param {array} asks   an array of current open asks
   def should_buy(trade_context)
-    # TODO
+    raise Exception, 'unimplemented method should_buy'
   end
 
   # if this method returns true then the trader should sell
@@ -59,30 +85,32 @@ class CexTrader
   # @param {array} bids   an array of current open bids
   # @param {array} asks   an array of current open asks
   def should_sell(trade_context)
-    # TODO
+    raise Exception, 'unimplemented method should_sell'
   end
 
   # Calculates the order that should be placed given the current
   # trader state.
   # @return {array} an array of [price, quantity]
   def calc_buy_order(trade_context)
-    #TODO
+    raise Exception, 'unimplemented method calc_buy_order'
   end
 
   # Calculates the order that should be placed given the current
   # trader state.
   # @return {array} an array of [price, quantity]
   def calc_sell_order(trade_context)
-    #TODO
+    raise Exception, 'unimplemented method calc_sell_order'
   end
 
   # places a sell order with the given price and amount
   def place_sell_order(price, amount)
+    @log.info "placing sell order for price: #{price}, amount: #{amount}")
     @api.place_order('sell', amount, price, @couple)
   end
 
   # places a buy order with the given price and amount
   def place_buy_order(price, amount)
+    @log.info "placing buy order for price: #{price}, amount: #{amount}")
     @api.place_order('buy', amount, price, @couple)
   end
 
