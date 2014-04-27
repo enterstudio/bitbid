90545eb99dc311ca18a7e92b118ad1e82c4cb414
fhur <fernando@authy.com>
2014-04-27 11:24:42 -0500

    Added several methods to TradeContext
    - bid_prices now returns a sorted list of bid prices, the same for
      ask_prices
    - created min_ask and max_bid methods
    - created spread method
    - improved the inspect function

--- trader/trade_context.rb
+++ trader/trade_context.rb
@@ -13,6 +13,7 @@ module Trader
       @trade_history = nil
       @balance = nil
       @active_orders = nil
+      @first_fetch = false
     end
 
     # returns the current open bids
@@ -26,11 +27,11 @@ module Trader
     end
 
     def bid_prices
-      bids.map {|bid| bid[0] }
+      bids.map {|bid| bid[0] }.sort
     end
 
     def ask_prices
-      asks.map {|ask| ask[0] }
+      asks.map {|ask| ask[0] }.sort
     end
 
     def bid_volumes
@@ -39,6 +40,24 @@ module Trader
 
     def ask_volumes
       asks.map {|ask| ask[1] }
+    end
+
+    # calculates the average price over the N cheapest asks
+    def min_ask(num=1)
+      prices = ask_prices.first(num)
+      return prices.reduce(:+)/prices.size
+    end
+
+    def max_bid(num=1)
+      prices = bid_prices.last(num)
+      return prices.reduce(:+)/prices.size
+    end
+
+    # Takes the N top bids and N lowest asks, calculates the average price for each one
+    # returns the difference
+    # @param {number} num the number of highest bids and lowest sells to compare
+    def spread(num=1)
+      max_bid(num) - min_ask(num)
     end
 
     # returns the users balance
@@ -56,6 +75,7 @@ module Trader
     end
 
     def fetch
+      @first_fetch = true
       @order_book = @api.order_book(@couple)
       @trade_history = @api.trade_history(1,@couple)
       @balance = @api.balance
@@ -64,7 +84,11 @@ module Trader
     end
 
     def inspect
-      "TradeContext bids:#{bids.size}, asks:#{asks.size}"
+      if not @first_fetch
+        "Trade Context - no nada"
+      else
+        "bids:#{bids.size}, asks:#{asks.size}, trade hist: #{trade_history.size}, max bid:#{bid_prices.max} , min ask:#{ask_prices.min}"
+      end
     end
   end
 
