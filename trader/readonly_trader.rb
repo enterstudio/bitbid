4391ed16d6b43e74f5056943df267c2c03506849
fhur <fernando@authy.com>
2014-04-28 22:46:00 -0500

    added method  to log the output of the readonly trader

--- trader/readonly_trader.rb
+++ trader/readonly_trader.rb
@@ -14,6 +14,16 @@ module Trader
       return ReadonlyTrader.new(log: log, sleep_period: sleep_period, trade_context: trade_context, api: api)
     end
 
+    def on_data_loaded(trade_context)
+      # fetch the average price of the 5 cheapest asks
+      # fetch the average price of the 5 highest bids
+      min_ask = trade_context.min_ask(5)
+      max_bid = trade_context.max_bid(5)
+      spread = trade_context.spread
+
+      @log.info "min5:#{min_ask}, max5:#{max_bid},spread:#{spread}"
+    end
+
     # never buy
     def should_buy(trade_context)
       return false
